6b180b9951e69cef5e7a149fec2a124458b9c85b
Anthony Gelder <anthony.gelder@gmail.com>
2020-04-10 21:02:53 -0700

    changed some things

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 console.log("load")
 
-var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbyFYOVHLR6XvYBvTm2O3CbCcsOZcBfQGwxW-HStmgZSxavVtZHe/exec'
+// var $form = $('form#test-form'),
+//     url = 'https://script.google.com/macros/s/AKfycbyFYOVHLR6XvYBvTm2O3CbCcsOZcBfQGwxW-HStmgZSxavVtZHe/exec'
 
-$('#submit-form').on('click', function(e) {
-    console.log('submit')
-    e.preventDefault();
-    var jqxhr = $.ajax({
-        url: url,
-        method: "GET",
-        dataType: "json",
-        data: $form.serializeObject()
-    }).success(
-        // do something
-    );
-})
+// $('#submit-form').on('click', function(e) {
+//     console.log('submit')
+//     e.preventDefault();
+//     var jqxhr = $.ajax({
+//         url: url,
+//         method: "GET",
+//         dataType: "json",
+//         data: $form.serializeObject()
+//     }).success(
+//         // do something
+//     );
+// })
 
+const quotes = [
+    "It's entirely possible.",
+    '100%',
+    'MMA is high level problem solving with dire physical consequences',
+    '11-hydroxy metabolite',
+    'heat shock protiens',
+    'If you ever start taking things too seriously, just remember that we are talking monkeys on an organic spaceship flying through the universe.',
+    'sensory deprivation chamber',
+    "There's only 2 reasons that you hate gay marriage; 1. You're dumb, or 2. You're secretly worried that dicks are delicious.",
+    'He got jjjacked, son',
+    'Pull that shit up, Jamie',
+    'I used to have a bit about that',
+    'I used to be addicted to Quake',
+    'I used to be addicted to pool',
+    'Jiu Jitsu is like human chess',
+    'You should start a podcast',
+    'We live in strange times',
+    'The problem is everyone wants to be on a team',
+    'The problem is no single person should be in charge',
+    'Being the president is an impossible job',
+    'More tigers are in captivity in Texas than the entire rest of the world',
+]
